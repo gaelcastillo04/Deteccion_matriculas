35cf9e9444153ea721aa2a3f369854701354ec8d
gaelcastillo04 <a01638638@tec.mx>
2025-05-07 16:37:08 -0600

    If applied, this commit will add the main code of the project.

---
+++ main.py
@@ -0,0 +1,50 @@
+import cv2
+import easyocr
+import numpy as np
+import sys
+
+# Inicializamos el EasyORC
+reader = easyocr.Reader(['es'])
+
+# Array de nombres y matriculas aceptados
+nombres=["OCTAVIO SEBASTIÁN HERNÁNDEZ GALINDO", "GAEL CASTILLO ZEPEDA"]
+matriculas=["A01638638","A01638993"]
+
+def procesado_imagen(nombre_archivo):
+    imagen =cv2.imread(nombre_archivo)
+    imagen_gray = cv2.cvtColor(imagen, cv2.COLOR_BGR2GRAY)
+    imagen_con_contraste = cv2.convertScaleAbs(imagen_gray, alpha=1.75, beta=-300)  # Alpha contraste y beta brillo
+    cv2.imwrite(f"{nombre_archivo}_contraste.png", imagen_con_contraste)
+    kernel= np.array([[-1,-1,-1],
+                     [-1,9,-1],
+                     [-1,-1,-1]])
+    imagen_con_sharpness=cv2.filter2D(imagen_con_contraste,-1,kernel)
+    cv2.imwrite(f"{nombre_archivo}_sharpening.png", imagen_con_sharpness)
+
+    # Utilizamos easyocr para detectar caracteres en imagenes
+    results=reader.readtext(imagen_con_sharpness, detail=0)  # detail = 0 retorno solo el texto
+    detected_text = " ".join(results).upper()
+    return detected_text
+
+
+def main():
+    if len(sys.argv)<2:
+        print("Error trata usando: python script.py nombre_imagen.jpg")
+        return
+    archivo=sys.argv[1]
+    resultado=procesado_imagen(archivo)
+    
+    # Filtrado por nombre y matricula
+    matricula_aprobada=[matricula for matricula in matriculas if matricula in resultado]
+    if matricula_aprobada:
+        # Si se confirma que la matricula concuerda checas el nombre del alumno
+        nombre_aprobado=[nombre for nombre in nombres if nombre in resultado]
+        if nombre_aprobado:
+            print(f"Entrada exitosa, Bienvenido: {nombre_aprobado[0]}")
+        else:
+            print("Entrada fallida, el nombre de la matricula no concuerda")
+    else:
+        print("Entrada fallida, la matricula no concuerda")
+
+if __name__ == "__main__":
+    main()
