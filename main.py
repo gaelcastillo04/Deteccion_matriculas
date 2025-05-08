3583c7a1a6936a250c70188f8b4cf1d73997c0ad
SEB-HDEZ-G <SEB-HDEZ-G@users.noreply.github.com>
2025-05-07 18:22:03 -0600

    if applied, this commit will fix line max lengths
    Maximum number of characters per line are now llimited to 72 for comments and 79 for code.

--- main.py
+++ main.py
@@ -3,26 +3,33 @@ import easyocr
 import numpy as np
 import sys
 
-# Inicializamos el EasyORC
+# Initialize ocr reader in spanish.
 reader = easyocr.Reader(['es'])
 
-# Array de nombres y matriculas aceptados
-nombres=["OCTAVIO SEBASTIÁN HERNÁNDEZ GALINDO", "GAEL CASTILLO ZEPEDA"]
-matriculas=["A01638638","A01638993"]
+# Arrays for accepted names and IDs.
+nombres = ["OCTAVIO SEBASTIÁN HERNÁNDEZ GALINDO", "GAEL CASTILLO ZEPEDA"]
+matriculas = ["A01638638", "A01638993"]
 
+# Function for image processing.
 def procesado_imagen(nombre_archivo):
-    imagen =cv2.imread(nombre_archivo)
+    imagen = cv2.imread(nombre_archivo)
     imagen_gray = cv2.cvtColor(imagen, cv2.COLOR_BGR2GRAY)
-    imagen_con_contraste = cv2.convertScaleAbs(imagen_gray, alpha=1.75, beta=-300)  # Alpha contraste y beta brillo
+    
+    # Alpha is used for contrast, while beta is used for brightness
+    imagen_con_contraste = cv2.convertScaleAbs(imagen_gray, 
+                                               alpha=1.75, 
+                                               beta=-300)
+    
     cv2.imwrite(f"{nombre_archivo}_contraste.png", imagen_con_contraste)
     kernel= np.array([[-1,-1,-1],
-                     [-1,9,-1],
-                     [-1,-1,-1]])
+                      [-1,9,-1],
+                      [-1,-1,-1]])
     imagen_con_sharpness=cv2.filter2D(imagen_con_contraste,-1,kernel)
     cv2.imwrite(f"{nombre_archivo}_sharpening.png", imagen_con_sharpness)
 
-    # Utilizamos easyocr para detectar caracteres en imagenes
-    results=reader.readtext(imagen_con_sharpness, detail=0)  # detail = 0 retorno solo el texto
+    # Using easyocr to detect characters in images
+    # If 'detail=0', then only the text is returned
+    results=reader.readtext(imagen_con_sharpness, detail=0)
     detected_text = " ".join(results).upper()
     return detected_text
 
@@ -35,9 +42,11 @@ def main():
     resultado=procesado_imagen(archivo)
     
     # Filtrado por nombre y matricula
-    matricula_aprobada=[matricula for matricula in matriculas if matricula in resultado]
+    matricula_aprobada=[matricula for matricula in matriculas if 
+                        matricula in resultado]
     if matricula_aprobada:
-        # Si se confirma que la matricula concuerda checas el nombre del alumno
+        # Si se confirma que la matricula concuerda checas el nombre 
+        # del alumno
         nombre_aprobado=[nombre for nombre in nombres if nombre in resultado]
         if nombre_aprobado:
             print(f"Entrada exitosa, Bienvenido: {nombre_aprobado[0]}")
